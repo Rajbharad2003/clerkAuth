e743b3e75c657407440387be6e5fedb4a27a645e
Rajbharad2003 <107258896+Rajbharad2003@users.noreply.github.com>
2024-05-30 00:39:09 +0530

    feat : cors implement.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 //import express from 'express';
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');   
 const app = express();
 require('dotenv').config();
 
 // Connect Database
 connectDB();
+
+const corsOptions = {
+    origin: '*', // Replace with your Angular app's URL http://localhost:4200
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use('/webHooks', require('./routes/webHooks'));
 
