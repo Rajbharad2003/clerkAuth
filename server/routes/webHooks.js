d8c981cfdc2f741766a293b78d7460ac5b33cdbf
Devanshu Chhipani <devanshuchhipani@gmail.com>
2024-05-31 00:23:16 +0530

    feat : changes in controller

--- server/routes/webHooks.js
+++ server/routes/webHooks.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+import bodyParser from "body-parser";
 const { syncClerkData } = require('../controllers/webHooksController.js');
 
-router.route("/createUpdateUser").post(syncClerkData);
+router.route("/createUpdateUser").post(bodyParser.raw({ type: "application/json" }),syncClerkData);
 
 module.exports = router;
